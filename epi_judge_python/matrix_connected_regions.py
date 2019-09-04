845a5ea4cda7e1e726a30c5009508e3d2c1f910a
Henry Rossiter <henry.rossiter@utexas.edu>
2019-09-04 06:56:48 -0500

    a few graph problems

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -1,8 +1,24 @@
 from test_framework import generic_test
-
+import collections
 
 def flip_color(x, y, image):
-    # TODO - you fill in here.
+    color = image[x][y]
+    q = collections.deque([(x, y)])
+    image[x][y] = not image[x][y]
+    while q:
+        x, y = q.popleft()
+        if 0 <= x + 1 < len(image) and 0 <= y < len(image[x + 1]) and image[x + 1][y] == color:
+            image[x + 1][y] = not image[x + 1][y]
+            q.append((x + 1, y))
+        if 0 <= x - 1 < len(image) and 0 <= y < len(image[x - 1]) and image[x - 1][y] == color:
+            image[x - 1][y] = not image[x - 1][y]
+            q.append((x - 1, y))
+        if 0 <= x < len(image) and 0 <= y + 1 < len(image[x]) and image[x][y + 1] == color:
+            image[x][y + 1] = not image[x][y + 1]
+            q.append((x, y + 1))
+        if 0 <= x < len(image) and 0 <= y - 1 < len(image[x]) and image[x][y - 1] == color:
+            image[x][y - 1] = not image[x][y - 1]
+            q.append((x, y - 1))
     return
 
 
--- epi_judge_python/search_maze.py
+++ epi_judge_python/search_maze.py
@@ -12,8 +12,20 @@ Coordinate = collections.namedtuple('Coordinate', ('x', 'y'))
 
 
 def search_maze(maze, s, e):
-    # TODO - you fill in here.
-    return []
+    def helper(cur):
+        if not (0 <= cur.x < len(maze) and 0 <= cur.y < len(maze[cur.x]) and maze[cur.x][cur.y] == WHITE):
+            return False
+        path_stack.append(cur)
+        maze[cur.x][cur.y] = BLACK
+        if cur == e:
+            return True
+        if any(map(helper, map(Coordinate, (cur.x - 1, cur.x + 1, cur.x, cur.x),(cur.y, cur.y, cur.y - 1, cur.y + 1)))):
+            return True
+        path_stack.pop()
+        return False
+    path_stack = []
+    helper(s)
+    return path_stack
 
 
 def path_element_is_feasible(maze, prev, cur):
